32ad4cdeb5aa621260adcca4c7360b790a4d15fe
Javed <50339361+JavedHussain-06@users.noreply.github.com>
2025-01-07 06:47:23 +0530

    added practice project

--- Practice/E-commerce/src/App.jsx
+++ Practice/E-commerce/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
-import products from "./db/data";
+import products from "./db/data.jsx";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
 import Card from "./components/Card";
